97c8c6a9dcccf629ef4703c64ee38854a5f82ccf
efondo-aaronkyle <akyleefondo@gmail.com>
2026-03-09 23:09:19 +0800

    fix files to test

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,58 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./auth/pages/Login";
-import Callback from "./auth/pages/Callback";
-import Unauthorized from "./auth/pages/Unauthorized";
-import ProtectedRoute from "./auth/components/ProtectedRoute";
-import OnePortalHome from "./features/one-portal/pages/OnePortalHome";
-import OnePortalProfile from "./features/one-portal/pages/Profile";
-import AppClient from "./features/idp/pages/AppClient";
-import UserPool from "./features/idp/pages/UserPool";
-import Roles from "./features/idp/pages/Roles";
-import IdpProfile from "./features/idp/pages/Profile";
-import IdpLayout from "./features/idp/layouts/IdpLayout";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Home from "./src/pages/Home";
 
 export default function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-
-        {/* Public Routes */}
-        <Route path="/" element={<Login />} />
-        <Route path="/callback" element={<Callback />} />
-        <Route path="/401" element={<Unauthorized />} />
-
-        {/* Protected IDP Routes */}
-        <Route
-          element={
-            <ProtectedRoute>
-              <IdpLayout />
-            </ProtectedRoute>
-          }
-        >
-          <Route path="/idp/app-client" element={<AppClient />} />
-          <Route path="/idp/user-pool" element={<UserPool />} />
-          <Route path="/idp/role" element={<Roles />} />
-          <Route path="/idp/profile" element={<IdpProfile />} />
-        </Route>
-
-        {/* Protected Portal Routes */}
-        <Route
-          path="/portal"
-          element={
-            <ProtectedRoute>
-              <OnePortalHome />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <OnePortalProfile />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/" element={<Home />} />
+        <Route path="/portal" element={<Home />} />
+        <Route path="/profile" element={<Navigate to="/portal" replace />} />
+        <Route path="*" element={<Navigate to="/portal" replace />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
