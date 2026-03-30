d71ed86d438e16c9832eee4569047d10ec42c1ce
causon-mikolorenz <109595939+causon-mikolorenz@users.noreply.github.com>
2026-03-30 22:12:01 +0800

    Feature/backend initialization (#11)
    * add go.mod and go.sum files with required dependencies
    
    * initialize backend with main server setup and graceful shutdown
    
    * add Dockerfile for multi-stage build and runtime configuration
    
    * add backend and database services to docker-compose configuration

---
+++ backend/cmd/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+const TimeOutDuration = 5 * time.Second
+
+func main() {
+	// Load environment variables from .env file
+	godotenv.Load()
+
+	// Create a context that listens for interrupt signals
+	ctx, stop := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
+	defer stop()
+
+	// Initialize the Gin router
+	r := gin.Default()
+
+	server := &http.Server{
+		Addr:    os.Getenv("BACKEND_PORT"),
+		Handler: r,
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatalf("[Main] Server Listen Error: %v", err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	stop()
+	log.Println("Shutting down gracefully...")
+
+	// Create a context with timeout for the shutdown process
+	shutdownCtx, cancel := context.WithTimeout(
+		context.Background(), 
+		TimeOutDuration,
+	)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("[Main] Forced Shutdown Error: %v", err)
+	}
+
+	log.Println("Server exited")
+}
