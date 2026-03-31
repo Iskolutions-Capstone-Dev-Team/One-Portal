a7b8166f63c57478464be97a2ebdf096f91aa0cc
causon-mikolorenz <causonmikolorenz@gmail.com>
2026-03-31 14:00:18 +0800

    Feature: add migration types for database schema management

---
+++ backend/internal/database/migrations/types.go
@@ -0,0 +1,16 @@
+package migrations
+
+type MigrationPart struct {
+	Name string
+	SQL  string
+}
+
+type MigrationStep struct {
+	ID  string 
+	SQL string
+}
+
+type TableMigration struct {
+	TableName string
+	Steps     []MigrationStep
+}
