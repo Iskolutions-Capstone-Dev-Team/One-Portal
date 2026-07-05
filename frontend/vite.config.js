371efc406b79d1cfa90cfb55b3a72cfa25b76858
efondo-aaronkyle <akyleefondo@gmail.com>
2026-07-05 20:28:41 +0800

    implement cdn

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import compression from "vite-plugin-compression2";
+import { Plugin as importToCDN } from "vite-plugin-cdn-import";
 
 function getEnvDirectory() {
   const repoRoot = resolve(__dirname, "..");
@@ -48,14 +49,31 @@ export default defineConfig(({ mode }) => {
       react(),
       compression({ algorithm: "brotliCompress" }),
       compression({ algorithm: "gzip" }),
+      importToCDN({
+        modules: [
+          {
+            name: "react",
+            var: "React",
+            path: "https://cdn.jsdelivr.net/npm/react@19.2.3/umd/react.production.min.js",
+          },
+          {
+            name: "react-dom",
+            var: "ReactDOM",
+            path: "https://cdn.jsdelivr.net/npm/react-dom@19.2.3/umd/react-dom.production.min.js",
+          },
+          {
+            name: "axios",
+            var: "axios",
+            path: "https://cdn.jsdelivr.net/npm/axios@1.7.9/dist/axios.min.js",
+          }
+        ],
+      }),
     ],
     build: {
       rollupOptions: {
         output: {
           manualChunks(id) {
             if (id.includes("node_modules")) {
-              if (id.includes("react")) return "vendor-react";
-              if (id.includes("axios")) return "vendor-axios";
               return "vendor";
             }
           },
