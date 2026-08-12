51d446aea5cb89958bab09e146420e428cc59b97
efondo-aaronkyle <akyleefondo@gmail.com>
2026-08-12 12:59:58 +0800

    add empty component

---
+++ frontend/src/components/ui/sonner.jsx
@@ -0,0 +1,58 @@
+"use client"
+
+import { useTheme } from "next-themes"
+import { Toaster as Sonner } from "sonner";
+import { CircleCheckIcon, InfoIcon, TriangleAlertIcon, CircleAlertIcon, Loader2Icon } from "lucide-react"
+
+const Toaster = ({
+  ...props
+}) => {
+  const { theme = "system" } = useTheme()
+
+  return (
+    <Sonner
+      theme={theme}
+      className="toaster group"
+      icons={{
+        success: (
+          <CircleCheckIcon className="size-5 text-green-600 dark:text-green-500" />
+        ),
+        info: (
+          <InfoIcon className="size-5" />
+        ),
+        warning: (
+          <TriangleAlertIcon className="size-5" />
+        ),
+        error: (
+          <CircleAlertIcon className="size-5 text-red-600 dark:text-red-500" />
+        ),
+        loading: (
+          <Loader2Icon className="size-5 animate-spin" />
+        ),
+      }}
+      style={
+        {
+          "--normal-bg": "var(--popover)",
+          "--normal-text": "var(--popover-foreground)",
+          "--normal-border": "var(--border)",
+          "--error-bg": "var(--popover)",
+          "--error-text": "var(--popover-foreground)",
+          "--error-border": "var(--border)",
+          "--success-bg": "var(--popover)",
+          "--success-text": "var(--popover-foreground)",
+          "--success-border": "var(--border)",
+          "--border-radius": "var(--radius)"
+        }
+      }
+      toastOptions={{
+        classNames: {
+          toast: "cn-toast",
+          error: "!bg-red-50 !text-red-900 !border-red-200",
+          success: "!bg-emerald-50 !text-emerald-900 !border-emerald-200",
+        },
+      }}
+      {...props} />
+  );
+}
+
+export { Toaster }
