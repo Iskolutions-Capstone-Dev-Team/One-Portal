b203c5b3f42c101dfbc0782a6f616958d8741e4f
causon-mikolorenz <causonmikolorenz@gmail.com>
2026-08-30 08:28:29 +0800

    add UpdateUserEmail method to One-Portal repository interface and implementation

--- backend/internal/repository/user_repository.go
+++ backend/internal/repository/user_repository.go
@@ -12,6 +12,7 @@ type UserRepository interface {
 	CreateUser(ctx context.Context, user models.User) error
 	GetUserByID(ctx context.Context, id uuid.UUID) (models.User, error)
 	UpdateUser(ctx context.Context, id uuid.UUID, user models.User) error
+	UpdateUserEmail(ctx context.Context, id uuid.UUID, email string) error
 }
 
 type userRepository struct {
@@ -83,5 +84,22 @@ func (r *userRepository) UpdateUser(
 
 	_, err = r.db.ExecContext(ctx, q, user.FirstName, user.MiddleName,
 		user.LastName, user.NameSuffix, idBytes)
+	return err
+}
+
+func (r *userRepository) UpdateUserEmail(
+	ctx context.Context,
+	id uuid.UUID,
+	email string,
+) error {
+	q := `UPDATE users SET email = ?, username = ?, ` +
+		`updated_at = CURRENT_TIMESTAMP WHERE id = ?`
+
+	idBytes, err := id.MarshalBinary()
+	if err != nil {
+		return err
+	}
+
+	_, err = r.db.ExecContext(ctx, q, email, email, idBytes)
 	return err
 }
